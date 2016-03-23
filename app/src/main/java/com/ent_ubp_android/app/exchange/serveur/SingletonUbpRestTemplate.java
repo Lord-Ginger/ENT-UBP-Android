8a2bf9562d8a951398b002b75873a5969426dc05
CHESNE Axel <axel.chesne@gmail.com>
2016-03-23 18:57:03 +0100

    Added : we can add a new classroom (linked with the server)

--- app/src/main/java/com/ent_ubp_android/app/exchange/serveur/SingletonUbpRestTemplate.java
+++ app/src/main/java/com/ent_ubp_android/app/exchange/serveur/SingletonUbpRestTemplate.java
@@ -1,5 +1,6 @@
 package com.ent_ubp_android.app.exchange.serveur;
 
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -27,12 +28,20 @@ public class SingletonUbpRestTemplate {
         // ajouter le header avec le token
         RestTemplate restTemplate = new RestTemplate();
         //add the jackson message converter
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        ((SimpleClientHttpRequestFactory) restTemplate.getRequestFactory()).setConnectTimeout(10 * 1000);
+        ((SimpleClientHttpRequestFactory) restTemplate.getRequestFactory()).setReadTimeout(10 * 1000);
 
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
         //Make the request, marshaling the response from Json to an array of T
         return restTemplate.getForObject(url, responseType);
 
         // conserver le token retourné par le restTemplate
+    }
+
+    public <T> T postForObject(String url, Object object, Class<T> responseType){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        return restTemplate.postForObject(url, object, responseType);
     }
 
 
--- app/src/main/java/com/ent_ubp_android/app/fragment/classroom/ClassroomAddFragment.java
+++ app/src/main/java/com/ent_ubp_android/app/fragment/classroom/ClassroomAddFragment.java
@@ -1,27 +1,41 @@
 package com.ent_ubp_android.app.fragment.classroom;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 import com.ent_ubp_android.app.R;
+import com.ent_ubp_android.app.adapter.recyclerview.RecyclerViewClassroomAdapter;
+import com.ent_ubp_android.app.exchange.serveur.SingletonUbpRestTemplate;
+import com.ent_ubp_android.app.fragment.FragmentSwitcher;
 import com.ent_ubp_android.app.model.classroom.Classroom;
+import com.ent_ubp_android.app.model.classroom.ClassroomType;
+import com.ent_ubp_android.app.model.classroom.RoomCapacity;
 import com.ent_ubp_android.app.model.classroom.equipement.EquipmentType;
+import com.ent_ubp_android.app.model.classroom.equipement.Quantity;
+import com.ent_ubp_android.app.model.classroom.equipement.RoomEquipment;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.client.HttpClientErrorException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.ref.WeakReference;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//TODO: Add link with the server
 public class ClassroomAddFragment extends Fragment implements CompoundButton.OnCheckedChangeListener, Button.OnClickListener {
+
+    private WeakReference<HttpRequestTask> asyncTaskWeakRef;
 
     List<EquipmentType> listEquipmentType;
 
@@ -152,7 +166,78 @@ public class ClassroomAddFragment extends Fragment implements CompoundButton.OnC
         } else if (!isCheckType()){
             Toast.makeText(getActivity(), "Veuillez entrer au moins un type de salle", Toast.LENGTH_LONG).show();
         } else {
-            //  Classroom classroom = new Classroom(nomSalle.getText(), capacity.getText(), )
+            Set<ClassroomType> types = new HashSet<>();
+            if(checkBoxTypeCM.isChecked())
+                types.add(ClassroomType.CM);
+            if(checkBoxTypeTD.isChecked())
+                types.add(ClassroomType.TD);
+            if(checkBoxTypeTP.isChecked())
+                types.add(ClassroomType.TP);
+
+            RoomCapacity roomCapacity = new RoomCapacity(Integer.parseInt(capacity.getText().toString()));
+            Classroom classroom = new Classroom(nomSalle.getText().toString(), roomCapacity, types);
+
+            if(checkBoxEquipment1.isChecked())
+                classroom.addEquipment(new RoomEquipment(new EquipmentType(checkBoxEquipment1.getText().toString()), new Quantity(1)));
+            if(checkBoxEquipment2.isChecked())
+                classroom.addEquipment(new RoomEquipment(new EquipmentType(checkBoxEquipment2.getText().toString()), new Quantity(1)));
+
+            startNewAsyncTask(classroom);
+        }
+    }
+
+    //Make the AsyncTask
+    private void startNewAsyncTask(Classroom classroom){
+        HttpRequestTask asyncTask = new HttpRequestTask(this, classroom);
+        this.asyncTaskWeakRef = new WeakReference<>(asyncTask);
+        asyncTask.execute();
+    }
+
+    private boolean isAsyncTaskPending(){
+        return this.asyncTaskWeakRef != null &&
+                this.asyncTaskWeakRef.get() != null &&
+                !this.asyncTaskWeakRef.get().getStatus().equals(AsyncTask.Status.FINISHED);
+    }
+
+    /* ************************************************* */
+    /*  Inner Class make the transaction with the server */
+    /* ************************************************* */
+    private class HttpRequestTask extends AsyncTask<Void, Void, Classroom> {
+
+        private WeakReference<ClassroomAddFragment> fragmentWeakRef;
+        private Classroom classroom;
+
+        private HttpRequestTask(ClassroomAddFragment fragment, Classroom classroom){
+            this.fragmentWeakRef = new WeakReference<>(fragment);
+            this.classroom = classroom;
+        }
+
+        @Override
+        protected Classroom doInBackground(Void... params) {
+            try {
+                final String url = SingletonUbpRestTemplate.BASE_URL + "classroom";
+                SingletonUbpRestTemplate restTemplate = SingletonUbpRestTemplate.getRestTemplate();
+                return restTemplate.postForObject(url, this.classroom, Classroom.class);
+            }
+            catch (HttpClientErrorException e){
+                return null;
+            }
+            catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Classroom classroom) {
+            super.onPostExecute(classroom);
+
+            if(classroom != null){
+                Toast.makeText(getActivity(), "Salle de cours ajouté avec succés", Toast.LENGTH_SHORT).show();
+                FragmentSwitcher.startAnotherFragment(new ClassroomMainFragment());
+            } else{
+                Toast.makeText(getActivity(), "Erreur dans la saisie de la salle", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
